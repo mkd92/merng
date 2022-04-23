b8926d2db2939728daf464a1e3431aa6b1c39b9d
mkd92 <manikandan.m1116@gmail.com>
2022-04-23 19:16:55 +0530

    everything is working 2:22:17

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
+import React from "react";
+import { BrouserRouter as Router, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hi World!</h1>
-    </div>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Router>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
-import ApolloProvider from "./apolloProvider";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
+import ApolloProvider from "./ApolloProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(ApolloProvider);
